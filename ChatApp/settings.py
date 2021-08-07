8998f88a5c301bd80a0669db3d64108be25827ef
Kelvin <laikelvin8@gmail.com>
2021-08-07 10:54:27 +0100

    Finished setting up Django + React integration

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
+    'rest_framework',
+    'frontend.apps.FrontendConfig'
 ]
 
 MIDDLEWARE = [
---
+++ frontend/src/components/App.js
@@ -0,0 +1,16 @@
+import React,{Component} from "React";
+import {render} from "react-dom";
+
+export default class App extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return <h1>Testing react</h1>;
+    }
+}
+
+const appDiv = document.getElementById("app");
+
+render(<App/>, appDiv);

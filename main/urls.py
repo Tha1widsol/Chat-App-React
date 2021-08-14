20ba9b36d170105a26468d0d257166e645b37f0b
Kelvin <laikelvin8@gmail.com>
2021-08-14 09:14:29 +0100

    Made changes

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
-from . import views
+from .views import RoomView
 urlpatterns = [
-
-    path("home/",views.home_page,name="home_page"),
+    path("home",RoomView.as_view()),
+   
 ]

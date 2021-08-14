b97e284cc30b575783d0e8ca9adee1856d7770b6
Kelvin <laikelvin8@gmail.com>
2021-08-14 09:13:33 +0100

    Made changes to route

--- ChatApp/urls.py
+++ ChatApp/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',include("main.urls")), 
-    path('',include('frontend.urls'))
+    path('api/',include('main.urls'))
+
 ]

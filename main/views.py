ddabfb8d88bf32b77df366a93b4b97b5032dd931
Kelvin <laikelvin8@gmail.com>
2021-08-14 09:14:49 +0100

    Added RoomView class

--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 # Create your views here.
 
-def home_page(request):
-    return HttpResponse("<h1>Hello world</h1>")
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer

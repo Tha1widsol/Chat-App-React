07226927845af928e64032443e55f5f88a1c4ba6
Kelvin <laikelvin8@gmail.com>
2021-08-17 19:32:54 +0100

    Added frontend.urls

--- ChatApp/urls.py
+++ ChatApp/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('main.urls'))
+    path('api/',include('main.urls')),
+    path('',include('frontend.urls'))
 
 ]

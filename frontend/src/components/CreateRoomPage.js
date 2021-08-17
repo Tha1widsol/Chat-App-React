a40af168da68c675d81849c40f7f7c28b706dd25
Kelvin <laikelvin8@gmail.com>
2021-08-17 19:33:47 +0100

    Added CreateRoomPage.js

---
+++ frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,8 @@
+import React,{Component,component} from 'react'
+import {BrowserRouter as Router,Switch,Route,Link,Redirect} from "react-router-dom"
+import AboutPage from './AboutPage';
+export default function CreateRoomPage(){
+    return(
+        <p>Create room page</p>
+   );
+}

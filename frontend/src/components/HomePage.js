9efd4999425d0525c4d111c2e23cf21df6c5b85b
Kelvin <laikelvin8@gmail.com>
2021-08-17 19:34:03 +0100

    Added changes in HomePage.js

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,12 +1,19 @@
-import React,{Component,component} from 'react'
+import React,{Component} from 'react'
 import {BrowserRouter as Router,Switch,Route,Link,Redirect} from "react-router-dom"
 import AboutPage from './AboutPage';
+import CreateRoomPage from './CreateRoomPage';
+import RoomJoinPage from './RoomJoinPage';
+
 export default function HomePage(){
     return(
     <Router>
         <Switch>
-            <Route exact path='/'> <p>Home page</p></Route>
-            <Route path='/about' component={AboutPage}/>
+            <Route exact path="/">
+                <p>This is the homepage</p>
+            </Route>
+
+            <Route exact path='/join' component={RoomJoinPage}></Route>
+            <Route path='/create' component={CreateRoomPage}></Route>
         </Switch>
     </Router>);
 }

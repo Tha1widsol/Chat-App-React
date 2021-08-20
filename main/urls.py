961c7ec9eae4ce5e31b846e80bfb23ffab7be5aa
Kelvin <laikelvin8@gmail.com>
2021-08-20 09:08:51 +0100

    Added create-room path

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include
-from .views import RoomView
+from .views import RoomView,CreateRoomView
+
 urlpatterns = [
     path("home",RoomView.as_view()),
+     path('create-room', CreateRoomView.as_view())
    
 ]

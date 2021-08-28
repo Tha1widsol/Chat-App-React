e26e63221945e784655b9fdfad07919f0a2bbe03
Kelvin <laikelvin8@gmail.com>
2021-08-28 10:33:50 +0100

    Added get-room path and imported GetRoom view

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path,include
-from .views import RoomView,CreateRoomView
+from .views import RoomView,CreateRoomView,GetRoom
 
 urlpatterns = [
     path("home",RoomView.as_view()),
-     path('create-room', CreateRoomView.as_view())
+     path('create-room', CreateRoomView.as_view()),
+     path('get-room',GetRoom.as_view())
    
 ]

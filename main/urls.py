33fa2b21683d942748530121003b9c749aac5f47
Kelvin <laikelvin8@gmail.com>
2021-08-31 11:15:05 +0100

    Added join room path

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path,include
-from .views import RoomView,CreateRoomView,GetRoom
+from .views import RoomView,CreateRoomView,GetRoom,JoinRoom
 
 urlpatterns = [
     path("home",RoomView.as_view()),
      path('create-room', CreateRoomView.as_view()),
-     path('get-room',GetRoom.as_view())
+     path('get-room',GetRoom.as_view()),
+     path('join-room', JoinRoom.as_view())
    
 ]

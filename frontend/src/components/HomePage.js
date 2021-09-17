895b0e611304d09ea195a476f816c550b381481c
Kelvin <laikelvin8@gmail.com>
2021-09-17 12:04:21 +0100

    Added register and login buttons

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,21 +1,30 @@
 import React,{useState,useEffect} from 'react'
 import {BrowserRouter as Router,Switch,Route,Link,Redirect} from "react-router-dom"
+import RegisterPage from './RegisterPage';
+import LoginPage from './LoginPage';
 
 export default function HomePage(){
 
     function renderHomePage(){
 
         return (
-            <div onClick={() => console.log("hello")}  style={{cursor:'pointer'}}>
+            <div style={{textAlign:"center"}}>
                 <h1>Chat App</h1>
+                
+                <a href='/register'><button>Register</button></a>
+                <br/>
+                <br/>
+                <a href='/sign_in'><button>Login</button></a>
             </div>
         );
     }
 
-    return(
+    return (
     <Router>
         <Switch>
             <Route exact path='/' component={renderHomePage}></Route>
+            <Route path='/register' component={RegisterPage}></Route>
+            <Route path='/sign_in' component={LoginPage}></Route>
         
         </Switch>
     </Router>);

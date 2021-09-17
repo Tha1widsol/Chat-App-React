4e249973b99552b4d7674989c2d1eee153cdf128
Kelvin <laikelvin8@gmail.com>
2021-09-17 12:05:17 +0100

    Imported make_password function

--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import JsonResponse
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
 

7f9b521ce89ab24bc0a998a2cdc3927cef6ef3ab
Kelvin <laikelvin8@gmail.com>
2021-09-20 10:09:58 +0100

    Added UserViewSet API

--- main/views.py
+++ main/views.py
@@ -1,10 +1,21 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-from rest_framework import generics,status
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
 from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.http import JsonResponse
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
 
 # Create your views here.
+
+@api_view(['GET'])
+def UserViewSet(request):
+    queryset = User.objects.all()
+    serializer = UserSerializer(queryset,many = True)
+    return Response(serializer.data)
+
+
 

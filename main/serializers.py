1bc6bef70db4c9909480b802f5b1422af2a55f1d
Kelvin <laikelvin8@gmail.com>
2021-09-23 15:28:37 +0100

    Added register and login serializer classes

--- main/serializers.py
+++ main/serializers.py
@@ -1,13 +1,31 @@
-from rest_framework import serializers
+from rest_framework import serializers, viewsets
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id','username','password']
+        fields = ['id','username']
 
-    def create(self, validate_data):
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id','username','password')
+        extra_kwargs = {'password':{'write_only':True}}
+    
+    def create(self,validate_data):
         user = User.objects.create_user(**validate_data)
         Token.objects.create(user = user)
         return user
+    
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self,data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        
+        raise serializers.ValidationError("Incorrect Credentials")

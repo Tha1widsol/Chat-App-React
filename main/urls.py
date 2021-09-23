66a6de07248d53647b52089fe2d3d999d93918cf
Kelvin <laikelvin8@gmail.com>
2021-09-23 15:28:49 +0100

    Added register and login api paths

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,9 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-path('users',views.UserViewSet,name = "users")
+path('users',views.UserViewSet,name = "users"),
+path('auth/register',views.RegisterAPI.as_view()),
+path('auth/login',views.LoginAPI.as_view())
+
    
 ]

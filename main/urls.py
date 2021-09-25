f3488d95c3dafd58b1c57fcce083174b2923ba51
Kelvin <laikelvin8@gmail.com>
2021-09-25 15:19:03 +0100

    Added logout path using knox_views

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path,include
-from django.contrib.auth import views as auth_views
+from knox import views as knox_views
 from . import views
 
 urlpatterns = [
 path('users',views.UserViewSet,name = "users"),
 path('auth/register',views.RegisterAPI.as_view()),
 path('auth/login',views.LoginAPI.as_view()),
-path('auth/logout',views.Logout.as_view())
+path('auth/logout',knox_views.LogoutView.as_view(),name='knox_logout')
 
    
 ]

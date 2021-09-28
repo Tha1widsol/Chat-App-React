c92e9ce44e24e8f9ec67bbb47f30745a3e0b195a
Kelvin <laikelvin8@gmail.com>
2021-09-28 12:45:59 +0100

    Added Login fetch API call

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -8,9 +8,32 @@ export default function LoginPage() {
         const username = UsernameRef.current.value
         const password = PasswordRef.current.value
 
-        console.log(username)
-        console.log(password)
+        const requestOptions = {
+            method:'POST',
+            headers:{'Content-Type':'application/json'},
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            }),
+        };
+    
+            fetch('/api/auth/login',requestOptions)
+            .then(response => {
+                if(!response.ok){
+                    throw Error("Error")
+                
+                }
+                return response.json()
+            })
+    
+            .then((data) => console.log(data))
+            
+            .catch(error => {
+                console.log(error.message)
+            })
     }
+
+   
     
     return (
     <div style={{textAlign:"center"}}>

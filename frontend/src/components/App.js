079e3c24fbe7b68afa156c4279860bb76aa2930f
Kelvin <laikelvin8@gmail.com>
2021-09-30 17:55:30 +0100

    Added useEffect login functionality

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,10 +1,42 @@
-import React,{useState,useEffect,useRef}  from "React";
+import React,{useState,useEffect,useRef}  from "react";
 import {render} from "react-dom";
 import HomePage from "./HomePage";
 
 
 export default function App(){
+
+        const [user,setUser] = useState({
+                logged_in: localStorage.getItem('token') ? true : false,
+                username: ""
+            })
+
+            useEffect(() => {
+                if(user.logged_in){
+                    fetch('/api/current_user',{
+                        headers: {
+                            Authorization:`Token ${localStorage.getItem('token')}`
+                        }
+                    })
+        
+                    .then(response => response.json())
+                    .then(data => {
+                        setUser({username:data.username,logged_in:true})
+                        console.log(data)
+                    })
+                }
+               
+             },[])
+
+        function handleLogout(){
+                localStorage.removeItem('token');
+                setUser({ logged_in: false, username: '' });
+            }
+           
+        
+        
         return (<div className="center">
+                 {user.logged_in ? <button onClick={handleLogout}>Logout</button>: null}
+                 {user.logged_in ? <p>Logged in as, {user.username} </p> : null} 
                 <HomePage/>
                 </div>);
 }

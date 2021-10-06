ac65d4345c7e849c57b8a7751188e2f473b401ee
Kelvin <laikelvin8@gmail.com>
2021-10-06 17:17:18 +0100

    Removed useEffect module

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect,useRef} from 'react'
+import React,{useState,useRef} from 'react'
 import { useHistory } from "react-router-dom";
 import Messages from './Messages';
 

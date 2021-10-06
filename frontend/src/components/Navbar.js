cdc4b01a603f1eca4359d4c4759cff847a6f0482
Kelvin <laikelvin8@gmail.com>
2021-10-06 17:17:33 +0100

    Added Navbar.js

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar({user,handleLogout}) {
+    return (
+        <div id="navbar">
+            <a href='/'>Chat App</a>
+            <a href='/home'>Home</a>
+            <a href="/about" >About</a>
+            <a href="/contact" >Contact</a>
+            {user.logged_in ? <a href='/' onClick={handleLogout}>Logout</a> : <> <a href="/register" >Register</a>  <a href="/sign_in" >Login</a> </> }
+            {user.logged_in ? <a>Logged in as, {user.username} </a> : null} 
+
+        </div>
+    )
+}

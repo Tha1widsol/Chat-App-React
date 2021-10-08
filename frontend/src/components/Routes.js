8a0de46baa1449372db2726fdfbc67c8bd3059e8
Kelvin <laikelvin8@gmail.com>
2021-10-08 11:39:18 +0100

    Added user logged in conditional statement

--- frontend/src/components/Routes.js
+++ frontend/src/components/Routes.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom"
+import {BrowserRouter as Router,Switch,Route,Redirect} from "react-router-dom"
 import RegisterPage from './RegisterPage'
 import LoginPage from './LoginPage'
 import HomePage from './HomePage'
+import User from './User'
 
-export default function Routes() {
+export default function Routes({user}) {
+
     return (
-    
             <Router>
                 <Switch>
-                    <Route exact path='/' component={HomePage}></Route>
-                    <Route exact path='/home' component={HomePage}></Route>
+                <Route exact path={["/","/home"]} render = {() => {
+                        return user.logged_in ? (<User user = {user}/>) : HomePage()
+                    }}/>
                     <Route path='/register' component={RegisterPage}></Route>
                     <Route path='/sign_in' render={() => <LoginPage/>} />
                 

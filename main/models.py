620555f730e3a4438eda53c2eb39922c2d556530
Kelvin <laikelvin8@gmail.com>
2021-10-11 17:26:01 +0100

    Added Add model class

--- main/models.py
+++ main/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Add(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    friend = models.ForeignKey(User, related_name='friend',on_delete=models.CASCADE,blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.user)
+
+
+

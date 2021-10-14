a7a907bfcf5c5aa699ec93efc7c8f7a12a5d558e
Kelvin <laikelvin8@gmail.com>
2021-10-14 13:58:47 +0100

    Added UserNav.js

---
+++ frontend/src/components/UserNav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function UserNav() {
+    return (
+        <div>
+            <div id="user-nav">
+                <a href='/'>Chat</a>
+                <a href='/search'>Search</a>
+                <a href='/'>Requests</a>
+            </div>
+        
+        </div>
+    )
+
+   
+    
+}

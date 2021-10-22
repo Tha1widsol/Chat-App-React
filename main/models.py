a993136e21619c29ff692e8351051afd30b00e8c
Kelvin <laikelvin8@gmail.com>
2021-10-22 18:12:53 +0100

    Changed user field into a Foreign key

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Add(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     friend = models.ForeignKey(User, related_name='friend',on_delete=models.CASCADE,blank=True)
     created = models.DateTimeField(auto_now_add=True)
 

411c05e7455a2c6f524c320164eb336d0dc4f2c7
Kelvin <laikelvin8@gmail.com>
2021-10-22 18:13:02 +0100

    Added requests and friends path

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path,include
+from django.urls import path
 from knox import views as knox_views
 from . import views
 
@@ -8,6 +8,8 @@ path('current_user',views.currentUser,name="user"),
 path('search',views.SearchAPI.as_view(),name="search"),
 path('add',views.AddAPI.as_view(),name="added"),
 path('sent',views.GetSentAPI.as_view(),name="sent"),
+path('requests',views.GetRequestsAPI.as_view(),name="requests"),
+path('friends',views.GetFriendsAPI.as_view(),name="friends"),
 path('remove_request',views.RemoveRequest.as_view(),name="remove_request"),
 path('auth/register',views.RegisterAPI.as_view()),
 path('auth/login',views.LoginAPI.as_view()),

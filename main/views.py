6c0ebefc16eea40cea6f8b39101a05bc73efe30e
Kelvin <laikelvin8@gmail.com>
2021-10-29 18:42:54 +0100

    Added accept friend request api + get friends api view functions

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics,status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.serializers import Serializer
 from .models import FriendRequest, User
 from rest_framework.views import APIView
 from .serializers import *
@@ -32,10 +33,12 @@ class SendFriendRequestAPI(APIView):
     def post(self,request,userID,*args,**kwargs):
         from_user = request.user
         to_user = User.objects.get(id = userID)
-        friend_request,created = FriendRequest.objects.get_or_create(from_user = from_user,to_user = to_user)
+        friend_request = FriendRequest.objects.filter(from_user = from_user,to_user = to_user)
 
-        if created:
-              return Response(status = status.HTTP_200_OK)
+        if not(friend_request.exists()) and not(to_user in request.user.friends.all()):            
+              friend_request,created = FriendRequest.objects.get_or_create(from_user = from_user,to_user = to_user)
+              if created:
+                 return Response(status = status.HTTP_200_OK)
         
         return Response(status = status.HTTP_400_BAD_REQUEST)
 
@@ -57,6 +60,28 @@ class GetSentFriendRequests(APIView):
             serializer_class = UserSerializer((s.to_user for s in sent),many = True)
             return Response(serializer_class.data,status =  status.HTTP_200_OK)
 
+        return Response(status = status.HTTP_200_OK)
+
+class AcceptFriendRequestAPI(APIView):
+    def post(self,request,requestID,*args,**kwargs):
+        from_user = User.objects.get(id = requestID)
+
+        friend_request = FriendRequest.objects.get(to_user = request.user,from_user = from_user)
+
+        if friend_request.to_user == request.user:
+            friend_request.to_user.friends.add(friend_request.from_user)
+            friend_request.from_user.friends.add(friend_request.to_user)
+            friend_request.delete()
+            return Response(status = status.HTTP_200_OK)
+    
+        return Response(status = status.HTTP_400_BAD_REQUEST)
+
+class GetFriendsAPI(APIView):
+    def get(self,request,*args,**kwargs):
+        if request.user.friends:
+            serializer_class = UserSerializer(request.user.friends,many = True)
+            return Response(serializer_class.data,status = status.HTTP_200_OK)
+            
         return Response(status = status.HTTP_200_OK)
 
 class RemoveSentRequestAPI(APIView):

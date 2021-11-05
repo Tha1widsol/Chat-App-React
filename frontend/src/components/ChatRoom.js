a24f2cf7333dae10dd4704559806476738aec8f0
Kelvin <laikelvin8@gmail.com>
2021-11-05 10:39:22 +0000

    Added new changes

--- frontend/src/components/ChatRoom.js
+++ frontend/src/components/ChatRoom.js
@@ -1,61 +1,47 @@
 import React,{useEffect,useState,useRef} from 'react'
-import io from 'socket.io-client'
+import io from 'socket.io-client';
 import { useHistory } from "react-router-dom";
 
 export default function ChatRoom(props) {
     const username = props.match.params.username
 
-    const [state,setStaet] = useState({message:'',name:''})
-    const [chat,setChat] = useState([])
+   
+   const [yourID,setYourID] = useState();
+   const [messages,setMessages] = useState([])
+   const [message,setMessage] = useState("")
 
-  
    const socketRef = useRef()
 
-	useEffect(
-		() => {
-			socketRef.current = io.connect("http://localhost:4000")
-			socketRef.current.on("message", ({ name, message }) => {
-				setChat([ ...chat, { name, message } ])
-			})
-			return () => socketRef.current.disconnect()
-		},
-		[ chat ]
-	)
+	useEffect (() => {
+		socketRef.current = io.connect("/");
+        socketRef.current.on("your id", id => {
+            setYourID(id)
+        })
+        socketRef.current.on("message",(message) => {
+            recievedMessage(message)
+        })
+    })
 
-    const onTextChange = (e) => {
-        setStaet({...state,[e.target.name]:e.target.value})
+    function recievedMessage(message){
+        setMessages(oldMsgs => [...oldMsgs,message])
     }
 
+    function sendMessage(e){
+        e.preventDefault()
+        const messageObject = {
+            body:message,
+            id: yourID,
+        }
+        setMessage("")
+        socketRef.current.emit("send message",messageObject)
+    }
 
-  
+   function handleChange(e){
+       setMessage(e.target.value)
+   }
 
-    const MessageRef = useRef()
-    const NameRef = useRef()
 
     let history = useHistory()
-
-    const renderChat = () => {
-		return chat.map(({ name, message }, index) => (
-			<div key={index}>
-				<h3>
-					{name}: <span>{message}</span>
-				</h3>
-			</div>
-		))
-	}
-
-
-
-
-    const handleMessageSubmit = (e) => {
-        e.preventDefault()
-        const { name, message } = state
-		socketRef.current.emit("message", { name, message })
-		e.preventDefault()
-		setStaet({ message: "", name })
-
-    };
-
 
 
     useEffect (() => {
@@ -75,22 +61,38 @@ export default function ChatRoom(props) {
 
     })
 
-    
-
-
     return (
         <div>
         
             <h2>Chat room</h2>
             <p>{username}</p>
-            <form onSubmit = {handleMessageSubmit}>
-            <input type='text'  onChange={e => onTextChange(e)}  placeholder='Name...'/>
-            <input type='text' onChange={e => onTextChange(e)}  placeholder='Message...'/>
-            
-            <button>Submit</button>
-            </form>
+    
             <h1>Chat Log</h1>
-          {renderChat()}
+
+         {messages.map((message,index) => {
+             if(message.id === yourID){
+                    return (
+                        <div key={index}>
+                            <p>{message.body}</p>
+                        </div>
+                    )
+             }
+
+            return (
+                <div key= {index}>
+                    <p>sender: {message.body}</p>
+                </div>
+            )
+
+         })}
+
+        <form onSubmit={sendMessage}>
+            <input type='text' value={message} onChange={handleChange}  placeholder='Message...'/>
+            <button>Send</button>
+        </form>
+        
+
+
         </div>
     )
 }

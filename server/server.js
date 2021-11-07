9d26fa14b4464a973f2bea70d266c3808acedee2
Kelvin <laikelvin8@gmail.com>
2021-11-07 00:05:32 +0000

    Added server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,24 @@
-const express = require("express")
-const http = require("http")
-const app = express();
-const server = http.createServer(app)
-const socket = require("socket.io");
-const io = socket(server)
+const io = require('socket.io')(3000)
+const users = {}
 
-io.on("connection",socket => {
-    socket.emit("your id",socket.id);
-    socket.on("send message",body => {
-        io.emit("message",body)
+io.on('connection',socket => {
+    socket.on('new-user',name => {
+        users[socket.id] = name
+        socket.broadcast.emit('user-connected',name)
     })
+    socket.on('send-chat-message',message => {
+        socket.broadcast.emit('chat-message',{message: message, name:users[socket.id]})
+    })
+
+    socket.on('disconnect',() => {
+       socket.broadcast.emit('user-disconnected',users[socket.id])
+       delete users[socket.id] 
+    })
+
+    socket.on('user-typing',() => { 
+        socket.broadcast.emit('typing',users[socket.id])
+    })
+        
+  
 })
 
-server.listen(3000, () => console.log("Server is running on port 3000"));

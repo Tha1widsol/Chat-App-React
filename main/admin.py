cbabd586f58542e77a7387a0da2bc252718ac8b8
Kelvin <laikelvin8@gmail.com>
2021-11-19 12:02:10 +0000

    Registered Chat an ChatRoom to admin

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import User,FriendRequest
+from .models import User,FriendRequest,Chat,ChatRoom
 
 admin.site.register(User)
 admin.site.register(FriendRequest)
+admin.site.register(Chat)
+admin.site.register(ChatRoom)
 
 

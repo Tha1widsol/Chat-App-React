c2e9ba02bf61b6ab4c6f12ef99688d4026a81472
Kelvin <laikelvin8@gmail.com>
2021-11-23 01:15:03 +0000

    Made field name changes to ChatRoom and Chat

--- main/models.py
+++ main/models.py
@@ -9,12 +9,13 @@ class FriendRequest(models.Model):
     from_user = models.ForeignKey(User,related_name='from_user',on_delete=models.CASCADE)
     to_user = models.ForeignKey(User,related_name='to_user',on_delete=models.CASCADE)
 
-class Chat(models.Model):
-    messages = models.CharField(max_length=1000)
-    sender = models.ForeignKey(User,related_name='sender',on_delete=models.CASCADE)
-    room = models.ForeignKey('ChatRoom',on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(auto_now=True)
-
 
 class ChatRoom(models.Model):
     name = models.CharField(max_length=500)
+    
+class Chat(models.Model):
+    messages = models.TextField()
+    sender = models.ForeignKey(User,related_name='sender',on_delete=models.CASCADE)
+    room = models.ForeignKey(ChatRoom,on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now=True)
+

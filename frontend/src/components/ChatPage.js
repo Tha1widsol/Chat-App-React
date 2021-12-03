8996d33cb389f21c501b6e42f4a2e6804b0d602c
Kelvin <laikelvin8@gmail.com>
2021-12-03 15:49:38 +0000

    Added create room popup container

--- frontend/src/components/ChatPage.js
+++ frontend/src/components/ChatPage.js
@@ -1,10 +1,13 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState,useEffect,useRef} from 'react'
 import { useHistory } from "react-router-dom";
 import Errors from './Errors'
 
 export default function ChatPage() {
     const [users,setUsers] = useState([])
     const [errors,setErrors] = useState([])
+    const [popup,setPopup] = useState(false)
+
+    const MessageRef = useRef()
 
     let history = useHistory()
 
@@ -40,12 +43,34 @@ export default function ChatPage() {
            
         })
     }
-
+    
     return (
         <div>
             <Errors errors = {errors} />
+            <button id="add_room" onClick = {() => setPopup(true)}>Add room </button>
             <h2>Chat</h2>
 
+            {popup ? <div className = "popup"> 
+                            <div className = "close" onClick = {() => setPopup(false)}>&times;</div>
+                            <h1><u>Create Room</u></h1>
+                            <input type='text' ref = {MessageRef} placeholder='Room name...'/>
+                            <h2>Add friends</h2>
+
+                            <div id ="box"> 
+                                {users.map(user => {
+                                    return (
+                                        <div id="check-box-friends">
+                                            <p>{user.id}. {user.username}</p>
+                                            <input type="checkbox" id="check"/>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                           
+                            <button type="submit">Submit</button>
+                    </div>
+                : null}
+         
             {users.map(user => {
             return (
             <div>

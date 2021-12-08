d85a4da6922a286b7d401b22aea52abe064f6a56
Kelvin <laikelvin8@gmail.com>
2021-12-08 01:01:59 +0000

    Implemented chat rooms

--- server/server.js
+++ server/server.js
@@ -1,28 +1,21 @@
 const io = require('socket.io')(3000)
-const users = {}
 
 io.on('connection',socket => {
-    socket.on('new-user',name => {
-        users[name] = socket.id
-        socket.broadcast.emit('user-connected',name,socket.id)
+    socket.on('new-user',room => {
+        socket.join(room)
     })
     
-    socket.on('typing',(name,sender) => { 
-        socket.to(users[name]).emit('user-typing',sender)
+    socket.on('typing',(room,sender) => { 
+        socket.to(room).emit('user-typing',sender)
     })
 
-    socket.on('send-chat-message',(message,sender,name) => {
-            socket.to(users[name]).emit('chat-message',{message: message, sender:sender})
+    socket.on('send-chat-message',(message,sender,room) => {
+            socket.to(room).emit('chat-message',{message: message, sender:sender})
     })
-
+    
     socket.on('disconnect',() => {
        socket.broadcast.emit('user-disconnected',socket.id)
-       Object.entries(users).map(user => {
-          const [key,value] = user
-          if(value === socket.id) 
-                delete users[key]
-       })
-          
+      
     })
 
         

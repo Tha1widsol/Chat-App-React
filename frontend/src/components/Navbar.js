8f4663b4c1fade80cd7a8b8660b21606f0666cea
Kelvin <laikelvin8@gmail.com>
2022-01-01 21:28:10 +0000

    Removed about + contact nav buttons

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,8 +5,6 @@ export default function Navbar({user,handleLogout}) {
         <div id="navbar">
             <a href='/'>Chat App</a>
             <a href='/home'>Home</a>
-            <a href="/about" >About</a>
-            <a href="/contact" >Contact</a>
             {user.logged_in ? <a href='/' onClick={handleLogout}>Logout</a> : <> <a href="/register" >Register</a>  <a href="/login/" >Login</a> </> }
             {user.logged_in ? <a>Logged in as, {user.username} </a> : null} 
 

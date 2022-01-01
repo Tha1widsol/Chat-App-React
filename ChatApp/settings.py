7e194ebb5d1f13d8f539b222af15efa5ef752405
Kelvin <laikelvin8@gmail.com>
2022-01-01 11:12:59 +0000

    Added to settings.py

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
+    'corsheaders',
     'rest_framework',
     'frontend.apps.FrontendConfig',
     'knox'
@@ -59,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'ChatApp.urls'
